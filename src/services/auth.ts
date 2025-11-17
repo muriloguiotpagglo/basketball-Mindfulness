9aac1800b6102560ab4adb04142b83cb0cd8fc3e
felipeab-b <felipeabferreira22@gmail.com>
2025-11-17 13:50:43 -0300

    feat: pegar o deviceToken no front

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,11 +1,34 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { api } from './api';
+import messaging from '@react-native-firebase/messaging'
+
+export async function saveDeviceToken(userId: number) {
+  try {
+    await messaging().requestPermission();
+    const fcmToken = await messaging().getToken();
+    if (fcmToken) {
+      console.log('FCM token obtido!', fcmToken);
+      await api.post('/users/save-token',{
+        userId:userId,
+        token:fcmToken,
+      });
+      console.log("Device token salvo com sucesso no backend");
+    }
+  } catch (error) {
+    console.error("Erro ao salvar Device Token")
+  }
+}
 
 export async function login(email: string, senha: string) {
   const res = await api.post('/login', { email, senha });
   const token: string = res.data?.token;
   if (token) {
     await AsyncStorage.setItem('token', token);
+
+    const userId = res.data?.userId;
+    if (userId) {
+      await saveDeviceToken(userId);
+    }
   }
   return { token };
 }
@@ -32,3 +55,4 @@ export async function register(user: {
   const res = await api.post('/users', payload);
   return res.data;
 }
+
